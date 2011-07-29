826285e9d74a59a90e3b0a0d4a243bfc1d15dbae
David Maxson <scnerd@gmail.com>
2011-07-29 11:29:53 -0700

    Child Display GUI code added
    Here's the actual GUI that the child is
    attempting to display to in real-time.

---
+++ Picasso/ImgSecDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Picasso
+{
+    public partial class frmImgSecDisplay : Form
+    {
+        private Image mImg;
+        private Action UpdateDel;
+        private bool IsRunning = false;
+
+        public frmImgSecDisplay()
+        {
+            InitializeComponent();
+            UpdateDel = new Action(this.Update);
+        }
+
+        internal void DisplayImage(Bitmap Picture)
+        {
+            IsRunning = true;
+            mImg = Picture;
+            picDisplay.Image = mImg;
+            //this.Invoke(UpdateDel);
+            IsRunning = false;
+        }
+
+        public new void CloseSafe()
+        {
+            while (IsRunning) ;
+        }
+    }
+}
